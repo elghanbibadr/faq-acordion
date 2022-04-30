f491ddf90ba1243ac2b6c130b2da2de6ece225ae
badr ghanbiE <bghaE@gmail.com>
2022-04-30 20:41:59 +0000

    add parcel for production

---
+++ dist/index.b6cc30ab.js
@@ -0,0 +1,2 @@
+const arrow=document.querySelectorAll(".faq__arrow");arrow.forEach((e=>e.addEventListener("click",(e=>{e.target.parentElement.nextElementSibling.classList.toggle("faq__answers--active"),e.target.previousElementSibling.classList.toggle("faq__ques-active")}))));
+//# sourceMappingURL=index.b6cc30ab.js.map
